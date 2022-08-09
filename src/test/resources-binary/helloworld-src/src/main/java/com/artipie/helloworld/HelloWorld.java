2d147f2d0c3864056f8f290ce307b7233ebcbe1d
Aleksei Borodin <45274464+Swizbiz@users.noreply.github.com>
2022-08-09 09:23:59 +0300

    chore: license  (#251)
    * chore: update secret and path to files with license
    
    * chore: short license text for all classes

--- src/test/resources-binary/helloworld-src/src/main/java/com/artipie/helloworld/HelloWorld.java
+++ src/test/resources-binary/helloworld-src/src/main/java/com/artipie/helloworld/HelloWorld.java
@@ -1,3 +1,7 @@
+/*
+ * The MIT License (MIT) Copyright (c) 2020-2022 artipie.com
+ * https://github.com/artipie/maven-adapter/blob/master/LICENSE.txt
+ */
 package com.artipie.helloworld;
 
 import java.time.LocalDateTime;
